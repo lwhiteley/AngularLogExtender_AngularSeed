767cefe943e36e304ebe4b78716d33887441edae
ferron <ferronrsmith@gmail.com>
2014-01-07 09:00:56 -0400

    updated sample app to use log provider

--- app/js/app.js
+++ app/js/app.js
@@ -4,6 +4,12 @@
 // Declare app level module which depends on filters, and services
 var myApp = angular.module('myApp', ['ngRoute', 'log.extension.uo']);
 
+
+myApp.config([ 'logExProvider', function(logExProvider) {
+    logExProvider.enableLogging(true);
+}]);
+
+
 myApp.config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
     $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
